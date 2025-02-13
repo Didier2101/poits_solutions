908ac8844ad9172a9ced751cfef6c16e2b6f187a
Didier Chávez <didierchavez2101@gmail.com>
2025-02-13 16:43:39 -0500

    Initial commit2

--- app/(dashboard)/colaboradores/[id]/page.tsx
+++ app/(dashboard)/colaboradores/[id]/page.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 
 import { useState, useEffect } from "react";
 import { useParams } from "next/navigation"; // Hook para obtener los parámetros dinámicos
@@ -8,34 +7,46 @@ import Link from "next/link";
 import Perfil from "@/components/dashboard/Perfil";
 
 export default function IdPage() {
-    const params = useParams(); // Hook para obtener `params`
-    const [colaborador, setColaborador] = useState<any>(null);
+  const params = useParams(); // Hook para obtener `params`
+  interface Colaborador {
+    id: string;
+    nombre: string;
+    cargo: string;
+    descripcion: string;
+    foto: string;
+    linkedin: string;
+    tecnologias: string[];
+    experiencia: string;
+    educacion: string;
+    certificaciones: string[];
+    logros: string[];
+  }
 
-    useEffect(() => {
-        if (params?.id) {
-            // Busca al colaborador por ID
-            const encontrado = colaboradores.find((colab) => colab.id === params.id);
-            setColaborador(encontrado);
-        }
-    }, [params]);
+  const [colaborador, setColaborador] = useState<Colaborador | null>(null);
 
-    // Si el colaborador no se encuentra (o los datos aún están cargando)
-    if (!colaborador) {
-        return (
-            <div className="p-6">
-                <h1 className="text-2xl font-bold">Colaborador no encontrado</h1>
-                <p>El ID proporcionado no corresponde a ningún colaborador registrado.</p>
-                <Link href="/colaboradores" className="text-teal-500 hover:underline">
-                    Volver
-                </Link>
-            </div>
-        );
+  useEffect(() => {
+    if (params?.id) {
+      // Busca al colaborador por ID
+      const encontrado = colaboradores.find((colab) => colab.id === params.id);
+      setColaborador(encontrado || null);
     }
+  }, [params]);
 
-    // Renderiza el perfil del colaborador
+  // Si el colaborador no se encuentra (o los datos aún están cargando)
+  if (!colaborador) {
     return (
-        <Perfil
-            colaborador={colaborador}
-        />
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Colaborador no encontrado</h1>
+        <p>
+          El ID proporcionado no corresponde a ningún colaborador registrado.
+        </p>
+        <Link href="/colaboradores" className="text-teal-500 hover:underline">
+          Volver
+        </Link>
+      </div>
     );
+  }
+
+  // Renderiza el perfil del colaborador
+  return <Perfil colaborador={colaborador} />;
 }
